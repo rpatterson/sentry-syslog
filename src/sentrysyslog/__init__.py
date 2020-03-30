20a5ca037404704a3b3930be9654bdca6ba47e98
Ross Patterson <me@rpatterson.net>
2020-03-29 21:19:02 -0700

    feat(cli): Add CLI options

--- src/sentrysyslog/__init__.py
+++ src/sentrysyslog/__init__.py
@@ -1,7 +1,9 @@
 """
-Python project structure foundation or template, top-level package.
+Send syslog messages to Sentry as events.
 """
 
+import sys
+import logging
 import argparse
 
 # Manage version through the VCS CI/CD process
@@ -12,12 +14,72 @@ except ImportError:  # pragma: no cover
 if version is not None:  # pragma: no cover
     __version__ = version.version
 
+
+def logging_level_type(level_name):
+    """
+    Lookup the logging level corresponding to the named level.
+    """
+    try:
+        level = getattr(logging, level_name)
+    except Exception as exc:
+        raise argparse.ArgumentTypeError(
+            "Could not look up logging level from name:\n{}".format(exc.args[0])
+        )
+    if not isinstance(level, int):
+        raise argparse.ArgumentTypeError(
+            "Level name {!r} doesn't correspond to a logging level, got {!r}".format(
+                level_name, level
+            )
+        )
+
+    looked_up_level_name = logging.getLevelName(level)
+    if looked_up_level_name != level_name:
+        raise argparse.ArgumentTypeError(
+            (
+                "Looked up logging level {!r} "
+                "doesn't match the given level name {!r}"
+            ).format(level, level_name)
+        )
+
+    return level
+
+
 # Define command line options and arguments
 parser = argparse.ArgumentParser(description=__doc__.strip())
+parser.add_argument(
+    "--input-file",
+    "-i",
+    type=argparse.FileType("r"),
+    default=sys.stdin,
+    help="Take the syslog messages from this file, one per-line. (default: stdin)",
+)
+parser.add_argument(
+    "--event-level",
+    "-e",
+    type=logging_level_type,
+    default=logging.ERROR,
+    help=(
+        "Capture log messages of this level and above as Sentry events.  "
+        "All other events are captured as Sentry breadcrumbs. "
+        "(default: %(default)s)"
+    ),
+)
+
+
+def run(
+    input_file=parser.get_default("input_file"),
+    event_level=parser.get_default("event_level"),
+):
+    pass
+
+
+run.__doc__ = __doc__
 
 
 def main(args=None):
-    parser.parse_args(args=args)
+    args = parser.parse_args(args=args)
+    with args.input_file:
+        return run(**vars(args))
 
 
 main.__doc__ = __doc__
--- src/sentrysyslog/tests/test_sentrysyslog.py
+++ src/sentrysyslog/tests/test_sentrysyslog.py
@@ -3,7 +3,9 @@ sentry-syslog unit and integration tests.
 """
 
 import contextlib
+import logging
 import io
+import tempfile
 import unittest
 
 import sentrysyslog
@@ -43,7 +45,10 @@ class SentrySyslogTests(unittest.TestCase):
         """
         The command line script accepts options controlling behavior.
         """
-        result = sentrysyslog.main(args=[])
+        with tempfile.NamedTemporaryFile() as input_file:
+            result = sentrysyslog.main(
+                args=["--input-file", input_file.name, "--event-level", "CRITICAL"]
+            )
         self.assertIsNone(
             result, "Wrong console script options return value",
         )
@@ -52,9 +57,38 @@ class SentrySyslogTests(unittest.TestCase):
         """
         The command line script displays useful messages for invalid option values.
         """
-        stderr = self.getCliErrorMessages(args=["--non-existent-option"])
+        stderr = self.getCliErrorMessages(args=["--input-file=__non_existent_file__"])
         self.assertIn(
-            "error: unrecognized arguments: --non-existent-option",
+            "No such file or directory: '__non_existent_file__'",
             stderr,
-            "Wrong invalid option message",
+            "Wrong invalid --input-file option message",
+        )
+
+        stderr = self.getCliErrorMessages(args=["--event-level=getLogger"])
+        self.assertIn(
+            "doesn't correspond to a logging level",
+            stderr,
+            "Wrong invalid --event-level option message",
+        )
+
+        stderr = self.getCliErrorMessages(
+            args=["--event-level=__non_existent_logging_module_attribute__"]
+        )
+        self.assertIn(
+            "Could not look up logging level",
+            stderr,
+            "Wrong invalid --event-level option message",
+        )
+
+        logging.__non_level_int_attribute__ = 999
+        try:
+            stderr = self.getCliErrorMessages(
+                args=["--event-level=__non_level_int_attribute__"]
+            )
+        finally:
+            del logging.__non_level_int_attribute__
+        self.assertIn(
+            "doesn't match the given level name",
+            stderr,
+            "Wrong non-level integer attribute --event-level option message",
         )
